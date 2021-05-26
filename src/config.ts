b898830e0af6ed47b81b881e8a730788b6a0ebdc
guillex <guillesiglez387@gmail.com>
2021-05-26 01:16:53 +0100

    I forget commit the app in production mode

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { app } from 'electron';
 import path from 'path';
 export const appPath: string = app.getAppPath();
-export const production: boolean = false;
+export const production: boolean = true;
 export const dataFolder: string = production ? path.join(appPath, '..', '..', 'data') : path.join(__dirname, '..', 'data');
 export const controllerPath: string = path.join(dataFolder, 'disks.json');
 export const disksFolder: string = path.join(dataFolder, 'disks');
