ffc71fd021b9d0409cd811a22d31911839c64d58
Guillex387 <guillesiglez387@gmail.com>
2022-10-15 20:51:07 +0100

    I correct the disk interface

--- src/lib/disk/block.ts
+++ src/lib/disk/block.ts
@@ -1,26 +1,26 @@
 import { BufferToUInt, UIntToBuffer } from '../../utils/binNums';
-import DiskInterface from './diskInterface';
+import DiskInterface, { INDICATOR_SIZE } from './diskInterface';
 import Error from '../error';
 
 class Block {
-  public static NULL_POINTER = Buffer.alloc(8);
+  public static NULL_POINTER = Buffer.alloc(INDICATOR_SIZE);
   public id: number;
   public disk: DiskInterface;
 
   get dataFrame(): Buffer {
-    let dataOffset = this.disk.BLOCK_SIZE * this.id + DiskInterface.INDICATOR_SIZE + this.disk.HEADER_SIZE;
+    let dataOffset = this.disk.BLOCK_SIZE * this.id + INDICATOR_SIZE + this.disk.HEADER_SIZE;
     return this.disk.read(dataOffset, this.length);
   }
   set dataFrame(b: Buffer) {
     if (b.length > this.disk.BLOCK_DATA_SIZE) throw new Error(4);
-    let dataOffset = this.disk.BLOCK_SIZE * this.id + DiskInterface.INDICATOR_SIZE + this.disk.HEADER_SIZE;
+    let dataOffset = this.disk.BLOCK_SIZE * this.id + INDICATOR_SIZE + this.disk.HEADER_SIZE;
     this.length = b.length;
     this.disk.write(b, dataOffset);
   }
 
   get length() {
     let lengthOffset = this.disk.BLOCK_SIZE * this.id + this.disk.HEADER_SIZE;
-    let lengthBuffer = this.disk.read(lengthOffset, 8);
+    let lengthBuffer = this.disk.read(lengthOffset, INDICATOR_SIZE);
     return BufferToUInt(lengthBuffer);
   }
   set length(l: number) {
@@ -29,15 +29,13 @@ class Block {
   }
 
   get next(): Block | null {
-    let pointerOffset =
-      this.disk.BLOCK_SIZE * this.id + this.disk.HEADER_SIZE + DiskInterface.INDICATOR_SIZE + this.disk.BLOCK_DATA_SIZE;
-    let pointerBuffer = this.disk.read(pointerOffset, 8);
+    let pointerOffset = this.disk.BLOCK_SIZE * this.id + this.disk.HEADER_SIZE + INDICATOR_SIZE + this.disk.BLOCK_DATA_SIZE;
+    let pointerBuffer = this.disk.read(pointerOffset, INDICATOR_SIZE);
     if (pointerBuffer.compare(Block.NULL_POINTER) === 0) return null;
     return new Block(BufferToUInt(pointerBuffer), this.disk);
   }
   set next(b: Block | null) {
-    let pointerOffset =
-      this.disk.BLOCK_SIZE * this.id + this.disk.HEADER_SIZE + DiskInterface.INDICATOR_SIZE + this.disk.BLOCK_DATA_SIZE;
+    let pointerOffset = this.disk.BLOCK_SIZE * this.id + this.disk.HEADER_SIZE + INDICATOR_SIZE + this.disk.BLOCK_DATA_SIZE;
     if (!b) this.disk.write(Block.NULL_POINTER, pointerOffset);
     else this.disk.write(UIntToBuffer(b.id), pointerOffset);
   }
--- src/lib/disk/diskInterface.ts
+++ src/lib/disk/diskInterface.ts
@@ -2,34 +2,32 @@ import * as fs from 'fs';
 import Error from '../error';
 import { BufferToUInt, UIntToBuffer } from '../../utils/binNums';
 
-abstract class DiskInterface {
-  public static INDICATOR_SIZE: number = 8;
+export const INDICATOR_SIZE = 8;
+interface DiskI {
+  BLOCK_SIZE: number;
+  BLOCK_DATA_SIZE: number;
+  HEADER_SIZE: number;
+  name: string;
+  pass: string;
+  metadata(): Object;
+  size(): number;
+  read(offset: number, length: number): Buffer;
+  write(buffer: Buffer, offset: number): void;
+  append(buffer: Buffer): void;
+  destructor(): void;
+}
+
+export class DiskFile implements DiskI {
+  private fd: number;
+  public file: string;
   public BLOCK_SIZE: number;
   public BLOCK_DATA_SIZE: number;
   public HEADER_SIZE: number;
+  public name: string;
   public pass: string;
 
-  get metadata(): Object {
-    return {};
-  }
-
-  size(): number {
-    return 0;
-  }
-  read(offset: number, length: number): Buffer {
-    return Buffer.alloc(0);
-  }
-  write(buffer: Buffer, offset: number) {}
-  append(buffer: Buffer) {}
-  destructor() {}
-}
-
-export class DiskFile extends DiskInterface {
-  private fd: number;
-  public file: string;
-
-  get metadata(): Object {
-    let headerLengthBuffer = this.read(0, DiskInterface.INDICATOR_SIZE);
+  metadata(): Object {
+    let headerLengthBuffer = this.read(0, INDICATOR_SIZE);
     let headerLength = BufferToUInt(headerLengthBuffer);
     let headerString = this.read(8, headerLength).toString('utf-8');
     return JSON.parse(headerString);
@@ -71,34 +69,34 @@ export class DiskFile extends DiskInterface {
     fs.closeSync(this.fd);
   }
 
-  static create(file: string, pass: string, metadata: Object) {
+  static create(file: string, name: string, pass: string, metadata: Object) {
     let metadataBuffer = Buffer.from(JSON.stringify(metadata), 'utf-8');
     let defaultBuffer = [
       UIntToBuffer(metadataBuffer.length),
       metadataBuffer,
-      Buffer.alloc(metadata['fragment-size'] + 2 * DiskInterface.INDICATOR_SIZE),
-      Buffer.alloc(metadata['fragment-size'] + 2 * DiskInterface.INDICATOR_SIZE),
+      Buffer.alloc(metadata['fragment-size'] + 2 * INDICATOR_SIZE),
+      Buffer.alloc(metadata['fragment-size'] + 2 * INDICATOR_SIZE),
     ];
     try {
       fs.writeFileSync(file, Buffer.concat(defaultBuffer));
     } catch (error) {
       throw new Error(2);
     }
-    return new DiskFile(file, pass);
+    return new DiskFile(file, name, pass);
   }
 
-  constructor(file: string, pass: string) {
-    super();
+  constructor(file: string, name: string, pass: string) {
     try {
       this.fd = fs.openSync(file, 'r+');
     } catch (error) {
       throw new Error(2);
     }
+    this.name = name;
     this.pass = pass;
-    this.HEADER_SIZE = DiskInterface.INDICATOR_SIZE + BufferToUInt(this.read(0, DiskInterface.INDICATOR_SIZE));
+    this.HEADER_SIZE = INDICATOR_SIZE + BufferToUInt(this.read(0, INDICATOR_SIZE));
     this.BLOCK_DATA_SIZE = this.metadata['fragment-size'];
-    this.BLOCK_SIZE = this.BLOCK_DATA_SIZE + 2 * DiskInterface.INDICATOR_SIZE;
+    this.BLOCK_SIZE = this.BLOCK_DATA_SIZE + 2 * INDICATOR_SIZE;
   }
 }
 
-export default DiskInterface;
+export default DiskI;
