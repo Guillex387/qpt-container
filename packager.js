0b6f49b7e3d7dba8d40491365bf04f6ecbd26ed3
Guillex387 <guillesiglez387@gmail.com>
2022-11-16 13:12:44 +0000

    New pre-release 2.0.0

--- packager.js
+++ packager.js
@@ -2,6 +2,7 @@ const packager = require('electron-packager');
 const path = require('path');
 const fs = require('fs-extra');
 const { execSync } = require('child_process');
+const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json')));
 
 // Prepare the temp enviroment
 console.log('Building env...');
@@ -10,24 +11,31 @@ const tempEnvDir = path.join(__dirname, 'tmp');
 if (fs.existsSync(tempEnvDir)) fs.rmSync(tempEnvDir, { recursive: true, force: true });
 fs.mkdirSync(tempEnvDir);
 fs.copySync(path.join(__dirname, 'core'), path.join(tempEnvDir, 'core'));
-fs.copySync(path.join(__dirname, 'package.json'), path.join(tempEnvDir, 'package.json'));
-execSync("npm install --omit=dev", { cwd: tempEnvDir });
+fs.writeFileSync(
+  path.join(tempEnvDir, 'package.json'),
+  JSON.stringify({
+    name: packageJson.name,
+    version: packageJson.version,
+    description: packageJson.description,
+    main: packageJson.main,
+    dependencies: packageJson.dependencies,
+  })
+);
+execSync('npm install --production', { cwd: tempEnvDir });
 
 // Package the app
 
 const defaultArchs = ['ia32', 'x64', 'arm64'];
 const defaultPlatforms = ['win32', 'linux'];
 let args = process.argv.slice(2);
-let target = (args.length === 1) ? args[0].split('-') : null;
+let target = args.length === 1 ? args[0].split('-') : null;
 let [targetPlatform, targetArch] = target || [null, null];
-
-let packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json')));
 
 console.log('Packing the app...');
 
 packager({
   appVersion: packageJson.version,
-  appCopyright: 'qpt-container Copyright (c) 2021 Guillex387. All rights reserved.',
+  appCopyright: 'qpt-container Copyright (c) 2022 Guillex387. All rights reserved.',
   arch: targetArch || defaultArchs,
   asar: true,
   buildVersion: packageJson.version,
